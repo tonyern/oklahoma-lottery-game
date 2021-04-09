2713b91fd4158dc664385c189deb959fa84d302f
Tony <tony.d.nguyen@protonmail.com>
2021-04-09 12:52:31 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OK_Lottery_Commission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
